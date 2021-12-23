cc781e9fbed63ff577a78d914cc23a0f5c451440
PimentelGabriel <gabrielcosta.tads@gmail.com>
2021-12-23 18:00:13 -0300

    Finalizado função de btn 1 e 2

--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
+window.popupForm = document.getElementById("popupForm");
+window.reclamar = document.getElementById("reclamar");
+
 document.querySelectorAll(".btn-close").forEach((btn) => {
     btn.addEventListener("click", (evt) => {
         evt.target.parentNode.parentNode.hidden = true;
     });
+});
+
+document.getElementById("btnSatelite1").addEventListener("click", (evt) => {
+    window.popupForm.hidden = !window.popupForm.hidden;
+});
+
+document.getElementById("btnSatelite2").addEventListener("click", (evt) => {
+    window.reclamar.hidden = !window.reclamar.hidden;
 });
